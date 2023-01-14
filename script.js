ebf58a2ac76d784d00245e36014629abb3b6e2c3
mateus-lopes <mateusalbano22@gmail.com>
2023-01-14 14:59:05 -0300

    add controler, fixed bugs, new layout

--- script.js
+++ script.js
@@ -5,24 +5,27 @@ const timerHour = document.querySelector('.hour');
 
 // const transition = document.querySelector('.transition');
 const startButton = document.querySelector('#start');
-const stopButton = document.querySelector('#stop');
 const resetButton = document.querySelector('#reset');
 
-let timer;
+let timer, controlerKey;
 let milisecond = 0;
 let second = 0;
 let minute = 0;
 let hour = 0;
 
-startButton.addEventListener('click', startTimer)
-stopButton.addEventListener('click', stopTimer)
+startButton.addEventListener('click', timerControler)
 resetButton.addEventListener('click', resetTimer)
 
 function addZero(n) {
     return n < 10 ? `0${n}` : n;
 };
 
-function startTimer() {
+function timerControler() {
+    return !controlerKey ? startTimer() : stopTimer()
+}
+
+function startTimer(){
+    controlerKey = true
     timer = setInterval(() => {
         milisecond+=1;
         if(milisecond >= 100){
@@ -41,18 +44,25 @@ function startTimer() {
         timerSecond.innerHTML = addZero(second)
         timerMinute.innerHTML = addZero(minute)
         timerHour.innerHTML = addZero(hour)
-        // transition.style.width = `${}px`
     }, 10);
+    startButton.innerHTML = 'STOP'
+    startButton.classList.add('btn-stop')
     console.log('start')
 }
 
 function stopTimer() {
+    controlerKey = false
     clearInterval(timer);
+    startButton.innerHTML = 'START'
+    startButton.classList.remove('btn-stop')
     console.log('stop')
 }
 
 function resetTimer() {
     [milisecond, second, minute, hour] = [0, 0, 0, 0]
-    startTimer()
+    timerMiliSecond.innerHTML = addZero(milisecond)
+    timerSecond.innerHTML = addZero(second)
+    timerMinute.innerHTML = addZero(minute)
+    timerHour.innerHTML = addZero(hour)
     console.log('reset')
 }
